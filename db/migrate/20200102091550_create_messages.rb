69ab08354546c4a0e92e842e0f792a1c2c9619ac
kota930 <fuji-whereismygi210@softbank.ne.jp>
2020-01-22 18:53:33 +0900

    テーブル名の変更

--- db/migrate/20200102091550_create_messages.rb
+++ db/migrate/20200102091550_create_messages.rb
@@ -1,6 +1,6 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
-    create_table :posts do |t|
+    create_table :messages do |t|
       t.string :content
       t.string :image
       t.references :group, foreign_key: true
